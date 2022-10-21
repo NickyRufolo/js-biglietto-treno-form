5ab51c2fc78ec68028343c62b46ff56ac7d0cb33
NikiRufolo <nikirufolo@gmail.com>
2022-10-21 18:36:56 +0200

    finish ticket

--- main.js
+++ main.js
@@ -19,6 +19,14 @@ function validation(){
             discount = 0 
             document.getElementById('ticket').innerHTML = KmPrice.toFixed(2) + `$`;
         }
+        //controllo quali campi ho lasciato vuoti
+        if (passengerName == `` || ValueAge == `` || ValueKm == ``) {
+            //codice che si attiva solo se tutti gli input sono vuoti
+            console.log(`compila tutto!`)
+            document.getElementById(`compila`).innerHTML = `Compila tutto!`;
+
+        } 
+
         // stampa - Id dato durante la stampa 
         document.getElementById(`passengernome`).innerHTML = passengerName;
         document.getElementById(`carrozza`).innerHTML = Math.floor(Math.random() * 10) +1;
@@ -28,4 +36,4 @@ function validation(){
         function resetta(){
             location.reload();
         }
-
+        
